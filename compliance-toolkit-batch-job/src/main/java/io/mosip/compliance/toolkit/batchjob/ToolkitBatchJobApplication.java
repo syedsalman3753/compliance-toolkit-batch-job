4d6c91bc4a7c4af504b8ed3255d876b4d76f6560
Mayura Deshmukh <52281514+mayuradesh@users.noreply.github.com>
2024-03-06 20:34:46 +0530

    Added auth adapater in classpath (#21)
    * MOSIP-24076 added CTK batch job
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * MOSIP-24076 added dockerfile
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * MOSIP-24076 added dockerfile
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * corrected jar file name
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * added server.servlet.context-path
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * MOSIP-24076 added auth adapter
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * MOSIP-24076 added servlet context path
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * MOSIP-24076 added management.endpoints
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * updated sequence
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * MOSIP-24076 updated bootstrap prop
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * corrected spring.application.name
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * fixing actuator issue
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * management.security.enabled
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * MOSIP-24076 updated dockerfile
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * added global.end-points
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * Added auth adapater in classpath
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    ---------
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    Signed-off-by: Mayura Deshmukh <52281514+mayuradesh@users.noreply.github.com>

--- compliance-toolkit-batch-job/src/main/java/io/mosip/compliance/toolkit/batchjob/ToolkitBatchJobApplication.java
+++ compliance-toolkit-batch-job/src/main/java/io/mosip/compliance/toolkit/batchjob/ToolkitBatchJobApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "io.mosip.compliance.toolkit.batchjob.*" })
+@ComponentScan(basePackages = { "io.mosip.compliance.toolkit.batchjob.*", "${mosip.auth.adapter.impl.basepackage}"})
 public class ToolkitBatchJobApplication {
 
 	public static void main(String[] args) {
